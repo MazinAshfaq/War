ee18c601837a0b694f13e44a9a068ec4e2d5cfaa
Mazin Ashfaq <mzash00@gmail.com>
2022-03-10 21:11:06 -0800

    Created deck and player classes

--- war.py
+++ war.py
@@ -1,28 +1,40 @@
 import random
 
 class Card:
-    def __init__(self,suit,value):
-        self.suit = suit
+    def __init__(self, suit, value):
         self.value = value
+        self.suit = suit
 
     def printCard(self):
-        print("{} of {}".format(self.value, self.suit))
+        print("{} of {}".format(self.value,self.suit))
 class Deck:
-    def __init__(self,cards):
-        self.cards = []
-        self.build()
-        
-    def build(self):
-        self.cards = list(range(2,13) * 4)
+    def __init__(self):
+        self.cards = list(range(2,15)) * 4
+        for suit in ['Hearts','Diamonds','Spades','Clubs']:
+            for value in range(1,14):
+                self.cards.append(Card(suit,value))
+
+        random.shuffle(self.cards)
+
+    def printDeck(self):
+        print(self.cards)
 class Player:
- def __init__(self):
+    def __init__(self,name):
+        self.wins = 0
+        self.card = None
+        self.name = name
         pass
 
 class War:
-     def __init__(self):
-        pass
+    def __init__(self):
+            self.deck = Deck()
+            self.player1 = Player("CPU")
+            self.player2 = Player(input("Enter your name: "))
+    
+    def start_war():
+        return
 
 
 if __name__ == "__main__":
-    card = Card("diamonds",5)
-    card.printCard()
+    game = War()
+    game.start_war()
