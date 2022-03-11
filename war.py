91b13490e654d8654cf77bae438a19300badc53c
Mazin Ashfaq <mzash00@gmail.com>
2022-03-10 19:23:51 -0800

    Build Card class with print function

--- war.py
+++ war.py
@@ -1,14 +1,28 @@
+import random
+
 class Card:
-   
+    def __init__(self,suit,value):
+        self.suit = suit
+        self.value = value
 
+    def printCard(self):
+        print("{} of {}".format(self.value, self.suit))
 class Deck:
-
+    def __init__(self,cards):
+        self.cards = []
+        self.build()
         
+    def build(self):
+        self.cards = list(range(2,13) * 4)
 class Player:
-
+ def __init__(self):
+        pass
 
 class War:
-    
+     def __init__(self):
+        pass
 
 
 if __name__ == "__main__":
+    card = Card("diamonds",5)
+    card.printCard()
